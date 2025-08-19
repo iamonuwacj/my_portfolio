eec73bddb239a4e0e7ec27da6068538d5f0684b7
iamonuwacj <onuwachinedu66@gmail.com>
2025-08-19 12:34:30 +0100

    updated projects section

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,3 +1,6 @@
+import clearfund from "../assets/clearfund.png"
+
+
 export  const projects = [
   {
     title: "Portfolio Website",
@@ -20,7 +23,7 @@ export  const projects = [
     description: `ClearFund aggregates past funding data from leading Web3 grant
                   platforms, solving the problem of fragmented information in decentralized
                   public goods funding.`,
-    image: "/src/assets/clearfund.png",
+    image: clearfund,
     tags: ["next.js", "firebase", "Tailwind", "firestore"],
     github: "https://github.com/LighthouseL2/Clearfund",
     demo: "https://clearfund.netlify.app"
